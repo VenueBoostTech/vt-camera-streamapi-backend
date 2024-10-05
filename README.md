818d8c89d5e3d60885ae17a9fc29a32bccf5accc
Griseld <development@venueboost.io>
2024-10-06 00:18:44 +0300

    feat: repo configuration for backend stream api

---
+++ README.md
@@ -0,0 +1,2 @@
+# Camera_Stream_API_Backend
+Backend for the camera stream api to save data logs in the database use some endpoints to get data.
---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "Camera_Stream_API_Backend",
+        script: "/home/ubuntu/miniconda3/bin/conda/envs/rtspStreamApp/bin/uvicorn",
+        args: "main:app --host 0.0.0.0 --port 8000",
+        interpreter: "/home/ubuntu/miniconda3/bin/conda/envs/rtspStreamApp/bin/python",
+        log_file: "/home/ubuntu/pm2_logs/fastapi-app.log",  // New log path
+        out_file: "/home/ubuntu/pm2_logs/fastapi-app-out.log",
+        error_file: "/home/ubuntu/pm2_logs/fastapi-app-error.log",
+      }
+    ]
+  };
