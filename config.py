1d3349cf2dedc18a15a6282bc6c27aa3b189a2fc
bmladva-developer <bmladva.developer@gmail.com>
2025-01-17 17:05:09 +0530

    Updated entry log api and minor change in zone API.

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./visiontrack.db"
+    DATABASE_URL: str = os.environ.get("DATABASE_URL")
     RTSP_STREAM_URL: str = ""
-    API_KEY: str = "your-api-key"
+    API_KEY: str = os.environ.get("API_KEY")
 
     class Config:
         env_file = ".env"
